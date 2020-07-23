1990cd60a3eb193bcbd480c7c301d3687db97bde
evgeniy_nikolaevich <zplacesound@gmail.com>
2020-07-23 09:49:20 +0300

    some changes /now it doesnt work/

--- main.php
+++ main.php
@@ -4,17 +4,49 @@ require_once ('settings.php');
 if (!file_exists('madeline.php')) {
     copy('https://phar.madelineproto.xyz/madeline.php', 'madeline.php');
 }
-require_once 'madeline.php';
+include 'madeline.php';
+
+
+
+function make_data_based_on_channels_list($channels){
+  $data = array(
+          'peer' => '@'.$channels[0], //название_канала, должно начинаться с @, например @breakingmash, все остальные параметры, кроме limit, можно оставить равными 0
+          'offset_id' => 0, 
+          'offset_date' => 0, 
+          'add_offset' => 0, 
+          'limit' => 10, //Количество постов, которые вернет клиент
+          'max_id' => 0, //Максимальный id поста
+          'min_id' => 0, //Минимальный id поста - использую для пагинации, при  0 возвращаются последние посты.
+          'hash' => 0
+          );
+  return $data;
+}
+
 
 
 function parse_telegram_channels($channels){
-  for($i=0;$i<count($channels);$i++){
-    echo $channels[$i];
-  }
+  $data = make_data_based_on_channels_list($channels);
+  return $data;
 }
 
 function main($channels){
-    parse_telegram_channels($channels);
+    $MadelineProto = new \danog\MadelineProto\API('session.madeline');
+    $MadelineProto->async(true);
+    $data = parse_telegram_channels($channels);
+    $MadelineProto->loop(function () use ($MadelineProto) {
+      yield $MadelineProto->start();
+  
+      $me = yield $MadelineProto->getSelf();
+  
+      $MadelineProto->logger($me);
     
+    if (!$me['bot']) {
+      $response = $MadelineProto->messages->getHistory($data);
+    }
+    
+  }
+
+);
 }
-main($telegram_channels);
+
+ main($telegram_channels);
